7c2896e6e48523ac953cf150cc9edb14622c2bc0
Gustavo Vinicius Dantas <guvinidantas@gmail.com>
2021-06-11 14:14:26 -0300

    reference correction

--- src/main/reducers.js
+++ src/main/reducers.js
@@ -2,12 +2,10 @@ import { combineReducers } from 'redux'
 import { reducer as formReducer } from 'redux-form'
 import { reducer as toastrReducer } from 'react-redux-toastr'
 
-import DashboardReducer from '../dashboard/dashboardReducer'
 import TabReducer from '../common/tab/tabReducer'
 import BillingCycleReducer from '../billingCycle/billingCycleReducer'
 
 const rootReducer = combineReducers({
-    dashboard: DashboardReducer,
     tab: TabReducer,
     billingCycle: BillingCycleReducer,
     form: formReducer,
